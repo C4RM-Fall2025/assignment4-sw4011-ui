ad64f71e8ffb7f83c7e8cde8e8f526c6dec3dde2
sw4011-ui <sw4011@columbia.edu>
2025-10-02 16:19:57 -0400

    Update BondPrice_File.py

--- BondPrice_File.py
+++ BondPrice_File.py
@@ -1,8 +1,10 @@
-
-
 def getBondPrice(y, face, couponRate, m, ppy=1):
-    if ppy == 1:
-        x = 2170604
-    if ppy == 2:
-        x = 2171686
-    return(x)
+    n = m * ppy
+    r = y / ppy
+    cf = face * couponRate / ppy
+    pv = 0
+    for i in range (1,n+1):
+        pv = pv + cf / (1 + r) ** i
+    
+    pvsum= pv + face / (1 + r) ** n
+    return(pvsum)
