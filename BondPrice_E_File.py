85f9305a6bf1611e38f4cbb379ce5a4c17f28f67
sw4011-ui <sw4011@columbia.edu>
2025-10-02 17:38:02 -0400

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,4 +1,18 @@
+def getBondPrice_E(face, couponRate, m, yc):
+    cf = face * couponRate
+    pv = 0
 
+    for i in range(1, m+1):
+        if i < m:
+            cashflow = cf
+        else:
+            cashflow = cf + face
 
-def getBondPrice_E(face, couponRate, yc):
-    return(2098949)
+        discount = 1.0
+        for k in range(i):
+            discount = (1 + yc[k]) ** i
+        discount = 1.0 / discount
+
+        pv += cashflow * discount
+    
+    return pv
