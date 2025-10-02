ea7139ffe30baa6dce949ad25075b84602e86691
sw4011-ui <sw4011@columbia.edu>
2025-10-02 16:42:40 -0400

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,3 +1,20 @@
-
 def getBondDuration(y, face, couponRate, m, ppy = 1):
-    return(8.51)
+    n = m * ppy
+    r = y / ppy
+    cf = face * couponRate / ppy
+    
+    pvcfSum = 0
+    pvcfTime = 0
+    
+    for i in range(1,n+1):
+        if i < n:
+            cashflow = cf
+        else:
+            cashflow = cf + face
+            
+        discount = 1 / (1 + r) ** i
+        pvcfSum = pvcfSum + cashflow * discount
+        pvcfTime = pvcfTime + i * cashflow * discount
+
+    getBondDuration = pvcfTime / pvcfSum / ppy
+    return(getBondDuration)
