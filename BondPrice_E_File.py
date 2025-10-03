c040f52a94303421685e6902d31c341be921188e
sw4011-ui <sw4011@columbia.edu>
2025-10-03 15:06:08 -0400

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,19 +1,13 @@
 def getBondPrice_E(face, couponRate, m, yc):
-    assert len(yc) == m
-    cf = face * couponRate
+    cf = face * couponRate 
     pv = 0
-
+    
     for i in range(1, m+1):
-        if i < m:
-            cashflow = cf
-        else:
-            cashflow = cf + face
-
-        discount = 1
-        for k in range(i):
-            discount = (1 + yc[k]) ** i
-        discount = 1 / discount
-
-        pv += cashflow * discount
+        cashflow = cf
+        if i == m:
+            cashflow += face
+        discount_factor = 1 / (1 + yc[i-1])**i
+        
+        pv += cashflow * discount_factor
     
     return pv
