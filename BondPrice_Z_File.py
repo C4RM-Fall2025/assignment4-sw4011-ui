19aad29376550dbe19f22d8a957fac2f8d5db021
sw4011-ui <sw4011@columbia.edu>
2025-10-03 16:12:57 -0400

    Update BondPrice_Z_File.py

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,4 +1,9 @@
-
-
-def getBondPrice_Z(face, couponRate, times, yc):
-    return(1996533)
+def getBondPrice_Z(face, couponRate,times, yc):
+    cf = face * couponRate 
+    pv = 0
+    for t,y in zip(times,yc):
+        if t == max(times):
+            pv += (face + cf) / (1 + y) ** t
+        else :
+            pv += cf / (1 + y) ** t
+    return(pv)
